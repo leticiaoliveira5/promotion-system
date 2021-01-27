65c620e65826e928706ff01b0728c3a7cf84e671
leticia <leoliveira994@gmail.com>
2021-01-26 21:40:35 -0300

    WIP

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -4,4 +4,27 @@ class PromotionsController < ApplicationController
         @promotions = Promotion.all 
     end
 
+    
+    def new
+        @promotion = Promotion.new
+    end
+
+    def show
+        @promotion = Promotion.find(params[:id])
+    end
+
+    def create
+    
+        @promotion = Promotion.new(params.require(:promotion).permit(:name, 
+        :description, :code, :discount_rate, :coupon_quantity, :expiration_date))
+        if @promotion.save
+            redirect_to promotion_path(@promotion)
+            else
+              redirect_to new_promotion_path
+              flash[:alert] = "Nenhuma promoção cadastrada"
+            end
+      
+    end
+
+
 end
