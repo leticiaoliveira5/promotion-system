9f42e5fc2be38ad72bf66635607003ab62c3b21e
leticia <leoliveira994@gmail.com>
2021-01-27 21:42:32 -0300

    WIP criar promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -3,7 +3,6 @@ class PromotionsController < ApplicationController
     def index
         @promotions = Promotion.all 
     end
-
     
     def new
         @promotion = Promotion.new
@@ -19,12 +18,8 @@ class PromotionsController < ApplicationController
         :description, :code, :discount_rate, :coupon_quantity, :expiration_date))
         if @promotion.save
             redirect_to promotion_path(@promotion)
-            else
-              redirect_to new_promotion_path
-              flash[:alert] = "Nenhuma promoção cadastrada"
-            end
+        end
       
     end
-
 
 end
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,6 @@
 class Promotion < ApplicationRecord
+
+    validates :name, :discount_rate, :coupon_quantity, :code, :expiration_date, presence: true
+
+
 end
